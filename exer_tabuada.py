66b04b937529d7579ca625a85d29e0fd7cf841da
larihlm04 <larih.lm04@gmail.com>
2026-02-25 22:47:46 -0300

    novos_arquivos

---
+++ exer_tabuada.py
@@ -0,0 +1,8 @@
+numero = int(input("Digite um numero: "))
+cont = 0
+valor = 0
+while cont <= 10:
+    cont = cont + 1
+    resultado = valor * numero
+    valor = valor + 1 
+    print(f"{valor} * {numero} = {resultado}")
